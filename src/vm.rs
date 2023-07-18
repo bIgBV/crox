93e21aca669e9dae40e8ab34d3412d8e8e2a5b63
bIgBV <bhargav.voleti93@gmail.com>
2023-07-17 18:02:06 -0700

    Make vm thread safe. Add tests

--- src/vm.rs
+++ src/vm.rs
@@ -1,5 +1,12 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{
+    ops::Deref,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        RwLock,
+    },
+};
 
+use scc::Stack;
 use thiserror::Error;
 use tracing::info;
 use zerocopy::FromBytes;
@@ -11,35 +18,49 @@ use crate::{
 
 #[derive(Debug)]
 pub struct Vm<'chunk> {
-    chunk: Option<&'chunk mut Chunk>,
+    chunk: RwLock<Option<&'chunk Chunk>>,
+    stack: Stack<Offset>,
     ip: AtomicUsize,
 }
 
 impl<'chunk> Vm<'chunk> {
     pub fn new() -> Self {
         Vm {
-            chunk: None,
+            chunk: RwLock::new(None),
+            stack: Stack::default(),
             ip: AtomicUsize::new(0),
         }
     }
 
-    pub fn interpret(&mut self, chunk: &'chunk mut Chunk) -> Result<(), VmError> {
-        self.chunk = Some(chunk);
+    pub fn interpret(&self, chunk: &'chunk Chunk) -> Result<(), VmError> {
+        let _ = self
+            .chunk
+            .write()
+            .expect("Unable to get write lock")
+            .insert(chunk);
         self.run()
     }
 
-    pub fn run(&mut self) -> Result<(), VmError> {
-        let mut chunk = self
+    pub fn run(&self) -> Result<(), VmError> {
+        if let Some(chunk) = *self
             .chunk
-            .take()
-            .expect("Run called with no chunk initialized");
+            .read()
+            .expect("Unable to obtain read lock on chunk")
+        {
+            self.run_loop(chunk)?
+        }
 
-        loop {
-            match self.read_byte(&chunk) {
+        Ok(())
+    }
+
+    fn run_loop(&self, chunk: &Chunk) -> Result<(), VmError> {
+        // TODO: Is Relaxed ordering here ok if we are AcqRel within the loop itself?
+        while self.ip.load(Ordering::Relaxed) < chunk.code.len() {
+            match self.read_byte(chunk) {
                 OpCode::Return => return Ok(()),
                 OpCode::Constant => {
                     let constant = self
-                        .read_type::<Offset>(&mut chunk)
+                        .read_type::<Offset>(chunk)
                         .and_then(|offset| chunk.get_value(offset))
                         .expect("No constant value found");
 
@@ -47,19 +68,21 @@ impl<'chunk> Vm<'chunk> {
                 }
             }
         }
+
+        Ok(())
     }
 
     fn read_byte(&self, chunk: &Chunk) -> OpCode {
         chunk.code[self.ip.fetch_add(1, Ordering::AcqRel)].into()
     }
 
-    fn read_type<T>(&self, chunk: &mut Chunk) -> Option<T>
+    fn read_type<T>(&self, chunk: &Chunk) -> Option<T>
     where
         T: Instruction + FromBytes,
     {
         let ip = self.ip.load(Ordering::Acquire);
         let instruction = ip + T::SIZE;
-        let val = T::read_from(&mut chunk.code[ip..instruction]);
+        let val = T::read_from(&chunk.code[ip..instruction]);
         self.ip.store(instruction, Ordering::Release);
 
         val
@@ -74,4 +97,36 @@ pub enum VmError {
 
     #[error("Runtime error")]
     Runtime,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn read_byte_updates_ip() {
+        let vm = Vm::new();
+        let mut chunk = Chunk::new("test");
+        chunk.write(OpCode::Return, 1);
+
+        let result = vm.interpret(&chunk);
+
+        assert!(result.is_ok());
+        assert_eq!(vm.ip.load(Ordering::Relaxed), 1);
+    }
+
+    #[test]
+    fn read_bytes_updates_ip() {
+        let vm = Vm::new();
+        let mut chunk = Chunk::new("test");
+        chunk.write_constant(6.8, 1).unwrap();
+
+        let result = vm.interpret(&chunk);
+
+        assert!(result.is_ok());
+        assert_eq!(
+            vm.ip.load(Ordering::Relaxed),
+            OpCode::Constant as usize + Offset::SIZE
+        );
+    }
 }
